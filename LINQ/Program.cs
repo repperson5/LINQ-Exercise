74ec075eaa79198f1691b64393a9e237387e7169
Rhea Epperson <re.epperson2@gmail.com>
2023-02-13 08:36:36 -0500

    exercise 1 complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var favMovieList = new List<string>() { "The Dark Knight", "Avatar 2", "Something Borrowed", "She's Out of Your League", "Harry Potter"};
+
+            //var listMovieOrdered = favMovieList.OrderBy(name => name.Length).ToList();
+
+            //foreach(var movie in listMovieOrdered)
+            //{
+            // Console.WriteLine(movie);
+            //}
+
+            //LINQ method
+
+            //listMovieOrdered.ForEach(Console.WriteLine);
+
+            //LINQ method concise
+
+            favMovieList.OrderBy(name => name.Length).ToList().ForEach(Console.WriteLine);
         }
     }
 }
